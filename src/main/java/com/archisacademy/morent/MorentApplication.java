15ebb77b8b93cf5ad1cea68e615458828ae22fa3
Ahmet Yeniçeri <yeniceriahmet02@gmail.com>
2024-11-29 21:38:14 +0300

    [NO-00] - setup project

---
+++ src/main/java/com/archisacademy/morent/MorentApplication.java
@@ -0,0 +1,13 @@
+package com.archisacademy.morent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archisacademy/morent/MorentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archisacademy.morent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
