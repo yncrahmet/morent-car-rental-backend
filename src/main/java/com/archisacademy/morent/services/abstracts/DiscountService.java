4814d9b0287afb296c3998c419e354d3c01e7d86
YusufGuducu <yusufguducu23@hotmail.com>
2024-12-19 17:09:44 +0300

    [OC-31] - Create-API-Apply-Discount

--- src/main/java/com/archisacademy/morent/services/abstracts/DiscountService.java
+++ src/main/java/com/archisacademy/morent/services/abstracts/DiscountService.java
@@ -1,9 +1,12 @@
 package com.archisacademy.morent.services.abstracts;
 
 
+import com.archisacademy.morent.dtos.requests.ApplyDiscountRequest;
 import com.archisacademy.morent.dtos.requests.CreateDiscountRequest;
+import com.archisacademy.morent.dtos.responses.ApplyDiscountResponse;
 import com.archisacademy.morent.dtos.responses.DiscountResponse;
 
 public interface DiscountService {
     DiscountResponse createDiscount(CreateDiscountRequest request);
+    ApplyDiscountResponse applyDiscount(ApplyDiscountRequest request);
 }
