c2c2212a85fd736744dab55cfc16d84717a7aa54
Ahmet Yeniçeri <yeniceriahmet02@gmail.com>
2025-01-24 16:02:05 +0300

    [NO-43] - Keycloak integrated.

--- src/main/java/com/archisacademy/morent/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/archisacademy/morent/SecurityConfig/SecurityConfig.java
@@ -1,31 +1,46 @@
 package com.archisacademy.morent.SecurityConfig;
 
 import com.archisacademy.morent.jwt.JwtAuthenticationFilter;
+import com.archisacademy.morent.jwt.JwtConverter;
 import com.archisacademy.morent.jwt.JwtService;
 import com.archisacademy.morent.jwt.TokenBlacklistService;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter authorizationFilter;
     private final AuthenticationProvider authenticationProvider;
     private final JwtService jwtService;
     private final TokenBlacklistService tokenBlacklistService;
+    private final JwtConverter jwtConverter;
 
+    public static final String ADMIN = "admin";
+    public static final String USER = "user";
     public static final String LOGIN = "/api/users/login";
     public static final String REGISTER = "/api/users/register";
+
+    public SecurityConfig(JwtAuthenticationFilter authorizationFilter, AuthenticationProvider authenticationProvider, JwtService jwtService, TokenBlacklistService tokenBlacklistService, JwtConverter jwtConverter) {
+        this.authorizationFilter = authorizationFilter;
+        this.authenticationProvider = authenticationProvider;
+        this.jwtService = jwtService;
+        this.tokenBlacklistService = tokenBlacklistService;
+        this.jwtConverter = jwtConverter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -33,13 +48,16 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request -> request
                         .requestMatchers(LOGIN, REGISTER, "/api/payments/initiate").permitAll()
                         .requestMatchers("/api/admin/register","/api/admin/login").permitAll()
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/api/user/**").hasRole("USER")
+                        .requestMatchers("/api/admin/**").hasAnyRole("ADMIN",ADMIN)
+                        .requestMatchers("/api/user/**").hasAnyRole("USER", USER)
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
+                    jwt.jwtAuthenticationConverter(jwtConverter);
+                }))
                 .logout(logout -> logout
                         .logoutUrl("/api/users/logout")
                         .logoutRequestMatcher(new AntPathRequestMatcher("/api/users/logout", "POST"))
